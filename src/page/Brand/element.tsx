477bb82d5bc9832e6a54a70899575f151c10d96a
AimHo <jjhwhwnsgh@gmail.com>
2020-06-28 00:57:44 +0900

    [update] snb 공통 component로 수정

--- src/page/Brand/element.tsx
+++ src/page/Brand/element.tsx
@@ -1,7 +1,47 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import useRouter from "../../hooks/useRouter";
 
 import * as S from "./style";
 import * as Icon from "../../icon";
+
+interface SnbProps {
+  list: {
+    label: string;
+    link: string;
+  }[];
+}
+const Snb = (props: SnbProps) => {
+  const { params } = useRouter();
+
+  const Button = () => {
+    return (
+      <React.Fragment>
+        {props.list.map((item, index) => {
+          const className = () => {
+            if (!params.type || item.link.indexOf(params.type) === -1)
+              return "";
+            return "active";
+          };
+          return (
+            <Link key={index} to={item.link} className={className()}>
+              {item.label}
+            </Link>
+          );
+        })}
+      </React.Fragment>
+    );
+  };
+
+  return (
+    <S.Snb>
+      <div className="btn-group">
+        <Button />
+      </div>
+    </S.Snb>
+  );
+};
 
 interface SlideCardProps {
   imgTags: any[];
@@ -467,6 +507,7 @@ const StoreCheonanWayToCome = () => {
 
 export {
   // 공통
+  Snb,
   TabNav,
   TabContentImage,
   // 팔복 - 인사말
--- src/page/Brand/index.tsx
+++ src/page/Brand/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import * as S from "./style";
+import useRouter from "../../hooks/useRouter";
+
 import Introduce from "./Introduce";
 import Story from "./Story";
 import Store from "./Store";
 
-import useRouter from "../../hooks/useRouter";
+import * as E from "./element";
+import * as S from "./style";
 
 const Brand = () => {
   const { params, linkTo } = useRouter();
@@ -18,24 +19,7 @@ const Brand = () => {
       { label: "진국스토리", link: "/brand/story" },
       { label: "매장안내", link: "/brand/store" },
     ];
-    return (
-      <S.Snb>
-        <div className="btn-group">
-          {list.map((item, index) => {
-            const className = () => {
-              if (!params.type || item.link.indexOf(params.type) === -1)
-                return "";
-              return "active";
-            };
-            return (
-              <Link key={index} to={item.link} className={className()}>
-                {item.label}
-              </Link>
-            );
-          })}
-        </div>
-      </S.Snb>
-    );
+    return <E.Snb list={list} />;
   };
 
   const Content = () => {
